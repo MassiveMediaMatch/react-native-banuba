e4b646be82f58d65d614d8a5ec274d0eaf8baef9
Jovan Stanimirovic <jovan@netlog.com>
2021-05-27 16:11:53 +0200

    fix music & overlay configuration iOS fix icons

--- ios/RNBanubaModule.swift
+++ ios/RNBanubaModule.swift
@@ -111,6 +111,7 @@ private func createVideoEditorConfiguration() -> VideoEditorConfig {
 	featureConfiguration.supportsTrimRecordedVideo = true
 	featureConfiguration.isAudioBrowserEnabled = false
 	featureConfiguration.isMuteCameraAudioEnabled = true
+	featureConfiguration.isDoubleTapForToggleCameraEnabled = true
 	config.updateFeatureConfiguration(featureConfiguration: featureConfiguration)
 	
 	config.isHandfreeEnabled = true
@@ -120,8 +121,8 @@ private func createVideoEditorConfiguration() -> VideoEditorConfig {
 	config.editorConfiguration = updateEditorConfiguration(config.editorConfiguration)
 //	config.combinedGalleryConfiguration = updateCombinedGalleryConfiguration(config.combinedGalleryConfiguration)
 //	config.videoCoverSelectionConfiguration = updateVideCoverSelectionConfiguration(config.videoCoverSelectionConfiguration)
-//	config.musicEditorConfiguration = updateMusicEditorConfigurtion(config.musicEditorConfiguration)
-//	config.overlayEditorConfiguration = updateOverlayEditorConfiguraiton(config.overlayEditorConfiguration)
+	config.musicEditorConfiguration = updateMusicEditorConfigurtion(config.musicEditorConfiguration)
+	config.overlayEditorConfiguration = updateOverlayEditorConfiguraiton(config.overlayEditorConfiguration)
 //	config.textEditorConfiguration = updateTextEditorConfiguration(config.textEditorConfiguration)
 	config.speedSelectionConfiguration = updateSpeedSelectionConfiguration(config.speedSelectionConfiguration)
 	config.handsfreeConfiguration = updateHandsFreeConfiguration(config.handsfreeConfiguration!)
@@ -148,12 +149,6 @@ private func updateRecorderConfiguration(_ configuration: RecorderConfiguration)
 		imageConfiguration: ImageConfiguration(imageName: "ic-beauty"),
 		selectedImageConfiguration: ImageConfiguration(imageName: "ic-beauty-on")
 	  ),
-//	  AdditionalEffectsButtonConfiguration(
-//		identifier: .sound,
-//		imageConfiguration: ImageConfiguration(imageName: "ic-mic"),
-//		selectedImageConfiguration: ImageConfiguration(imageName: "ic-mic-off"),
-//		position: .bottom
-//	  ),
 	  AdditionalEffectsButtonConfiguration(
 		identifier: .effects,
 		imageConfiguration: ImageConfiguration(imageName: "ic-filter"),
@@ -168,18 +163,6 @@ private func updateRecorderConfiguration(_ configuration: RecorderConfiguration)
 		identifier: .toggle,
 		imageConfiguration: ImageConfiguration(imageName: "ic-flip-camera"),
 		selectedImageConfiguration: ImageConfiguration(imageName: "ic-flip-camera")
-//		title: TextButtonConfiguration(
-//			style: TextConfiguration(
-//				font: UIFont.systemFont(ofSize: 12.0),
-//				color: UIColor(red: 255, green: 255, blue: 255).withAlphaComponent(1)
-//			),
-//			text: "Flip"
-//		)
-//		titlePosition: .bottom,
-//		width: 100,
-//		height: 100,
-//		position: .top,
-//		imageTitleSpacing: 0
 	  ),
 	  AdditionalEffectsButtonConfiguration(
 		identifier: .flashlight,
@@ -196,11 +179,6 @@ private func updateRecorderConfiguration(_ configuration: RecorderConfiguration)
 		imageConfiguration: ImageConfiguration(imageName: "ic-speed"),
 		selectedImageConfiguration: nil
 	  ),
-//	  AdditionalEffectsButtonConfiguration(
-//		identifier: .muteSound,
-//		imageConfiguration: ImageConfiguration(imageName: "ic-mic"),
-//		selectedImageConfiguration: ImageConfiguration(imageName: "ic_mic_off")
-//	  ),
 	]
 
 	configuration.speedButton = SpeedButtonConfiguration(
@@ -271,7 +249,7 @@ private func updateEditorConfiguration(_ configuration: EditorConfiguration) -> 
 	  AdditionalEffectsButtonConfiguration(
 		identifier: .sound,
 		imageConfiguration: ImageConfiguration(imageName: "ic-mic"),
-		selectedImageConfiguration: ImageConfiguration(imageName: "ic-mic-off"),
+		selectedImageConfiguration: ImageConfiguration(imageName: "ic-mic-on"),
 		position: .bottom
 	  ),
 	  AdditionalEffectsButtonConfiguration(
@@ -436,5 +414,262 @@ private func updateHandsFreeConfiguration(_ configuration: HandsfreeConfiguratio
 	configuration.timerOptionBarConfiguration.optionCornerRadius = 8
 	configuration.timerOptionBarConfiguration.sliderCornerRadius = 8
 	
+	return configuration
+}
+
+// MARK: - Overlay Editor (Gifs, Text)
+
+func updateOverlayEditorConfiguraiton(_ configuration: OverlayEditorConfiguration) -> OverlayEditorConfiguration {
+	var configuration = configuration
+	
+	configuration.mainOverlayViewControllerConfig = updateMainOverlayViewControllerConfig(configuration.mainOverlayViewControllerConfig)
+	configuration.interactivesConfig = nil
+	
+	return configuration
+  }
+  
+func updateMainOverlayViewControllerConfig(_ configuration: MainOverlayViewControllerConfig) -> MainOverlayViewControllerConfig {
+	var configuration = configuration
+
+	configuration.addButtons = [
+//	  OverlayAddButtonConfig(
+//		type: .text,
+//		title: "Text",
+//		titleColor: .white,
+//		font: UIFont.systemFont(ofSize: 14.0),
+//		imageName: "ic_AddText"
+//	  ),
+	  OverlayAddButtonConfig(
+		type: .sticker,
+		title: "GIFs",
+		titleColor: .white,
+		font: UIFont.systemFont(ofSize: 12.0),
+		imageName: "ic-gifs"
+	  )
+	]
+
+	configuration.editButtonsHeight = 50.0
+	configuration.editButtonsInteritemSpacing = 0.0
+
+	configuration.controlButtons = [
+	  OverlayControlButtonConfig(
+		type: .reset,
+		imageName: "ic_restart",
+		selectedImageName: nil
+	  ),
+	  OverlayControlButtonConfig(
+		type: .play,
+		imageName: "ic_editor_play",
+		selectedImageName: "ic_pause"
+	  ),
+	  OverlayControlButtonConfig(
+		type: .done,
+		imageName: "ic-done",
+		selectedImageName: nil
+	  )
+	]
+
+	configuration.editCompositionButtons = [
+	  OverlayEditButtonConfig(
+		type: .edit,
+		title: NSLocalizedString("OverlayEditor.Edit", comment: "edit comment"),
+		titleColor: .white,
+		font: UIFont.systemFont(ofSize: 14.0),
+		imageName: "ic-edit",
+		selectedImageName: nil
+	  ),
+	  
+	  OverlayEditButtonConfig(
+		type: .delete,
+		title: NSLocalizedString("OverlayEditor.Delete", comment: "delete comment"),
+		titleColor: .white,
+		font: UIFont.systemFont(ofSize: 14.0),
+		imageName: "ic-delete",
+		selectedImageName: nil
+	  )
+	]
+
+	configuration.playerControlsHeight = 50.0
+	configuration.mainLabelColors = #colorLiteral(red: 0.2350233793, green: 0.7372031212, blue: 0.7565478683, alpha: 1)
+	configuration.additionalLabelColors = UIColor.white
+	configuration.additionalLabelFonts = UIFont.systemFont(ofSize: 12.0)
+	configuration.cursorColor = UIColor.white
+	configuration.audioWaveConfiguration.borderColor = UIColor.white
+	configuration.resizeImageName = "ic_cut_arrow"
+	configuration.draggersHorizontalInset = .zero
+	configuration.draggersHeight = 25.0
+	configuration.backgroundConfiguration.cornerRadius = .zero
+	configuration.playerControlsBackgroundConfiguration.cornerRadius = .zero
+	configuration.defaultLinesCount = 2
+	configuration.timelineCornerRadius = .zero
+	configuration.draggerBackgroundColor = UIColor.white
+	configuration.timeLabelsOffset = .zero
+	configuration.itemsTopOffset = .zero
+
+	return configuration
+}
+
+// MARK: - Music Editor
+
+func updateMusicEditorConfigurtion(_ configuration: MusicEditorConfig) -> MusicEditorConfig {
+	var configuration = configuration
+
+	configuration.mainMusicViewControllerConfig = updateMainMusicViewControllerConfig(configuration.mainMusicViewControllerConfig)
+	configuration.audioRecorderViewControllerConfig = updateAudioRecorderViewControllerConfig(configuration.audioRecorderViewControllerConfig)
+	configuration.audioTrackLineEditControllerConfig = updateAudioTrackLineEditControllerConfig(configuration.audioTrackLineEditControllerConfig)
+	configuration.videoTrackLineEditControllerConfig = updateVideoTrackLineEditControllerConfig(configuration.videoTrackLineEditControllerConfig)
+
+	return configuration
+}
+
+func updateMainMusicViewControllerConfig(_ configuration: MainMusicViewControllerConfig) -> MainMusicViewControllerConfig {
+	var configuration = configuration
+
+	configuration.editButtonsHeight = 50.0
+	configuration.editButtons = [
+	  EditButtonConfig(
+		font: UIFont.systemFont(ofSize: 14.0),
+		type: .record,
+		title: NSLocalizedString("MusicEditor.Record", comment: "record comment"),
+		titleColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1),
+		imageName: "ic-mic-record"
+	  )
+	]
+	configuration.editCompositionButtons = [
+	  EditCompositionButtonConfig(
+		font: UIFont.systemFont(ofSize: 14.0),
+		type: .edit,
+		title: NSLocalizedString("OverlayEditor.Edit", comment: "edit comment"),
+		titleColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1),
+		imageName: "ic-edit",
+		selectedImageName: nil
+	  ),
+	  EditCompositionButtonConfig(
+		font: UIFont.systemFont(ofSize: 14.0),
+		type: .delete,
+		title: NSLocalizedString("OverlayEditor.Delete", comment: "delete comment"),
+		titleColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1),
+		imageName: "ic-delete",
+		selectedImageName: nil
+	  )
+	]
+
+	configuration.controlsBackgroundConfiguration.color = #colorLiteral(red: 0.1176470588, green: 0.1333333333, blue: 0.1607843137, alpha: 1)
+	configuration.controlsBackgroundConfiguration.cornerRadius = 0
+	configuration.playerControlsHeight = 40.0
+
+	configuration.controlButtons = [
+	  ControlButtonConfig(
+		type: .reset,
+		imageName: "ic-reset",
+		selectedImageName: nil
+	  ),
+	  ControlButtonConfig(
+		type: .play,
+		imageName: "ic-play",
+		selectedImageName: "ic-pause"
+	  ),
+	  ControlButtonConfig(
+		type: .done,
+		imageName: "ic-done",
+		selectedImageName: nil
+	  )
+	]
+
+	configuration.audioWaveConfiguration.isRandomWaveColor = true
+	configuration.mainLabelColors = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+	configuration.additionalLabelColors = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+//	configuration.speakerImageName = "imageName"
+//	configuration.volumeLabel.title = "Title"
+	configuration.tracksLimit = 5
+	configuration.cursorColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+	configuration.backgroundConfiguration.cornerRadius = 0.0
+	configuration.timelineCornerRadius = 0.0
+
+	return configuration
+}
+
+func updateAudioRecorderViewControllerConfig(_ configuration: AudioRecorderViewControllerConfig) -> AudioRecorderViewControllerConfig {
+	var configuration = configuration
+
+	configuration.playerControlsBackgroundConfiguration.color = #colorLiteral(red: 0.1176470588, green: 0.1333333333, blue: 0.1607843137, alpha: 1)
+	configuration.playerControlsBackgroundConfiguration.cornerRadius = 0.0
+	configuration.playerControlsHeight = 40
+
+	configuration.rewindToStartButton = ControlButtonConfig(
+	  type: .reset,
+	  imageName: "ic-reset",
+	  selectedImageName: nil
+	)
+
+	configuration.playPauseButton = ControlButtonConfig(
+	  type: .play,
+	  imageName: "ic-play",
+	  selectedImageName: "ic-pause"
+	)
+
+	configuration.recordButton = ControlButtonConfig(
+	  type: .play,
+	  imageName: "ic-mic",
+	  selectedImageName: "ic-mic-off"
+	)
+	
+	configuration.backButtonImage = "ic-close"
+	configuration.doneButtonImage = "ic-done"
+	configuration.
+	configuration.dimViewColor = #colorLiteral(red: 0.3176470588, green: 0.5960784314, blue: 0.8549019608, alpha: 0.2039811644)
+	configuration.additionalLabelColors = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+	configuration.startingRecordingTimerSeconds = 0.0
+	configuration.timerColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+	configuration.cursorColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+	configuration.backgroundConfiguration.cornerRadius = 0.0
+	configuration.playerControlsBackgroundConfiguration.cornerRadius = 0.0
+	configuration.timelineCornerRadius = 0.0
+
+	return configuration
+}
+
+func updateVideoTrackLineEditControllerConfig(_ configuration: VideoTrackLineEditViewControllerConfig) -> VideoTrackLineEditViewControllerConfig {
+	var configuration = configuration
+
+	configuration.doneButtonImageName = "ic-done"
+	configuration.doneButtonTintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+	configuration.sliderTintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+	configuration.mainLabelColors = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+	configuration.additionalLabelColors = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+	configuration.backgroundConfiguration.cornerRadius = 0.0
+	configuration.height = 50.0
+	return configuration
+}
+
+func updateAudioTrackLineEditControllerConfig(_ configuration: AudioTrackLineEditViewControllerConfig) -> AudioTrackLineEditViewControllerConfig {
+	var configuration = configuration
+
+	configuration.audioWaveConfiguration.isRandomWaveColor = true
+	configuration.audioWaveConfiguration.waveLinesColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+	configuration.doneButtonImageName = "ic-done"
+	configuration.doneButtonTintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+	configuration.sliderTintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+	configuration.draggersColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+//	configuration.draggerImageName = "trim_left"
+//	configuration.trimHeight = 61.0
+//	configuration.trimBorderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+//	configuration.trimBorderWidth = 2.0
+//	configuration.cursorHeight = 1.0
+//	configuration.dimViewColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+//	configuration.mainLabelColors = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+//	configuration.additionalLabelColors = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+//	configuration.cursorColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+//	configuration.draggersWidth = 25.0
+//	configuration.draggersLineColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+//	configuration.draggersCornerRadius = 0.0
+//	configuration.draggersLineWidth = 2.0
+//	configuration.draggersLineHeight = 35.0
+//	configuration.numberOfLinesInDraggers = 1
+//	configuration.draggerLinesSpacing = 2.0
+//	configuration.draggersCornerRadius = 0.0
+//	configuration.backgroundConfiguration.cornerRadius = 0.0
+//	configuration.voiceFilterConfiguration?.cornerRadius = 4.0
+
 	return configuration
 }
